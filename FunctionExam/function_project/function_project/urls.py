48fba052f0304e7c9b526c1119d047213dddb220
toumei-kurage2 <yoshikawa-r@hokugin-sw.co.jp>
2024-07-26 10:38:33 +0900

    Update urls.py

--- FunctionExam/function_project/function_project/urls.py
+++ FunctionExam/function_project/function_project/urls.py
@@ -14,16 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path,include,re_path
 from django.conf import settings
-from django.conf.urls.static import static
+from django.views.static import serve
 from accounts.views import show_error_page
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("accounts/",include("accounts.urls")),
     path("boards/",include("boards.urls")),
+    re_path(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}), 
 ]
 handler404=show_error_page
-
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
